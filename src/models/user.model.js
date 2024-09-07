2fb8bfecc31be64d7a909cf1f13521303d0ef2e4
bangrinaldy <bangrinaldy@gmail.com>
2024-09-07 22:22:35 +0700

    fix

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -28,10 +28,13 @@ module.exports = (sequelize, Sequelize) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        
+
+    },
+    { 
         // don't add the timestamp attributes (updatedAt, createdAt)
-        timestamps: false,
-    })
+        timestamps: false 
+    }
+    )
 
     return User;
 }
